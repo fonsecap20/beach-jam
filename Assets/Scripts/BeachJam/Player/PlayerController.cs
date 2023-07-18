2eda10bb5cf4dfc836f83d2f6b1c16390f948c0a
BiggHiggs <higgsthecat@gmail.com>
2023-07-17 23:27:19 -0400

    Added Editable Starting Coroutine
    added a starting coroutine that accelerates the player until they reach the starting speed, at which point the game can be started.

--- Assets/Scripts/BeachJam/Player/PlayerController.cs
+++ Assets/Scripts/BeachJam/Player/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed;
+    [Header("Starting Sequence Vars")]
+    public float startSpeed; //The starting velocity the player enters the scene with to accelerate from
+    public float defaultSpeed; //The speed the player will be at when the game finishes the starting sequence - this is not the default, min, or max speed
+    public float timeToMaxSpeed; //The amount of time in seconds it takes to reach endSpeed
+    public AnimationCurve startSpeedCurve; //Curve the acceleration will follow
 
     private Rigidbody2D rb;
 
@@ -12,11 +16,26 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(BeginLevel());
+    }
+
+    IEnumerator BeginLevel()
+    {
+        //Add WaitForSeconds part here if we want to do anything before the game starts
+        float elapsedTime = 0f; //Time the graph evaluation starts at
+        while (elapsedTime < timeToMaxSpeed)
+        {
+            float currentSpeed = Mathf.Lerp(startSpeed, defaultSpeed, startSpeedCurve.Evaluate(elapsedTime / timeToMaxSpeed)); //Scales by taking the value every frame until the X value of the graph is 1 (complete)
+            rb.velocity = Vector3.right * currentSpeed;
+            elapsedTime += Time.deltaTime; //Keeping this as unscaled
+            yield return null;
+        }
+        rb.velocity = Vector3.right * defaultSpeed; //This can probably be changed to Vector2
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = Vector3.right * movementSpeed;
+
     }
 }
